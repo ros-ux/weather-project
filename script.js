5a568854d06e1b71053956c934a214943402336c
ros-ux <56478795+ros-ux@users.noreply.github.com>
2020-01-20 13:46:42 -0500

    Removal of current location settings
    Removal to try to to fix code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function formatDate(date) {
   if (hours < 10) {
     hours = `0${hours}`;
   }
+
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -36,12 +37,6 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-function searchLocation(position) {
-  let apiKey = "fd5f09fc0b375ff07de082ff1808cae9";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
-}
-
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -55,19 +50,11 @@ function displayWeather(response) {
     response.data.weather[0].main;
 }
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
 let dateElement = document.querySelector("#date");
 let date = new Date();
 dateElement.innerHTML = formatDate(date);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocation = document.querySelector("#current-location");
-currentLocation.addEventListener("click", getCurrentLocation);
 
 searchCity("Quito");
